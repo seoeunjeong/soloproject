7dec3df399fcb0affec44c1be2335395708f1523
서은정 <111293023+seoeunjeong@users.noreply.github.com>
2023-09-15 19:55:57 +0900

    프로젝트 시작

---
+++ src/main/java/soloproject/seomoim/HomeController.java
@@ -0,0 +1,13 @@
+package soloproject.seomoim;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(){
+        return "hi";
+    }
+}
---
+++ src/main/java/soloproject/seomoim/SeomoimApplication.java
@@ -0,0 +1,13 @@
+package soloproject.seomoim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeomoimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeomoimApplication.class, args);
+	}
+
+}
