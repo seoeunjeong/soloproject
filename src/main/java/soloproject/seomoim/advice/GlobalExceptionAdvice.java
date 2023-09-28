07b8d5f5d5d45fa0e049d16cc6220d8df2b40789
서은정 <111293023+seoeunjeong@users.noreply.github.com>
2023-09-28 15:48:26 +0900

    [feat]예외처리 로직

--- src/main/java/soloproject/seomoim/advice/GlobalExceptionAdvice.java
+++ src/main/java/soloproject/seomoim/advice/GlobalExceptionAdvice.java
@@ -3,7 +3,9 @@ package soloproject.seomoim.advice;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import soloproject.seomoim.exception.BusinessLogicException;
 import soloproject.seomoim.exception.ErrorResponse;
@@ -12,10 +14,14 @@ import soloproject.seomoim.exception.ErrorResponse;
 public class GlobalExceptionAdvice {
 
     @ExceptionHandler
-    public ErrorResponse handleHttpRequestMethodNotSupportedException(HttpRequestMethodNotSupportedException e) {
-        ErrorResponse response = ErrorResponse.of(HttpStatus.METHOD_NOT_ALLOWED, e.getMessage());
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleMethodArgumentNotValidException(
+            MethodArgumentNotValidException e) {
+        final ErrorResponse response = ErrorResponse.of(e.getBindingResult());
+
         return response;
     }
+
     @ExceptionHandler
     public ResponseEntity handleBusinessLogicException(BusinessLogicException e) {
         ErrorResponse errorResponse = ErrorResponse.of(e.getExceptionCode());
--- src/main/java/soloproject/seomoim/exception/ErrorResponse.java
+++ src/main/java/soloproject/seomoim/exception/ErrorResponse.java
@@ -4,6 +4,12 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+
+import java.lang.reflect.Field;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 @Setter
@@ -11,6 +17,8 @@ public class ErrorResponse{
 
     private int status;
     private String message;
+    private List<FieldError> fieldErrors;
+
 
     private ErrorResponse(int status, String message) {
         this.status = status;
@@ -21,14 +29,37 @@ public class ErrorResponse{
         return new ErrorResponse(exceptionCode.getStatus(), exceptionCode.getMessage());
     }
 
-    public static ErrorResponse of(HttpStatus httpStatus){
-        return new ErrorResponse(httpStatus.value(), httpStatus.getReasonPhrase());
-
-    }
-    public static ErrorResponse of(HttpStatus httpStatus, String message) {
-        return new ErrorResponse(httpStatus.value(), message);
+    private ErrorResponse(List<FieldError> fieldErrors){
+        this.fieldErrors=fieldErrors;
     }
 
+    public static ErrorResponse of(BindingResult bindingResult) {
+        return new ErrorResponse(FieldError.of(bindingResult));
+    }
 
+    @Getter
+    public static class FieldError {
+        private String field;
+        private Object rejectedValue;
+        private String reason;
 
+        private FieldError(String field, Object rejectedValue, String reason) {
+            this.field = field;
+            this.rejectedValue = rejectedValue;
+            this.reason = reason;
+        }
+
+        public static List<FieldError> of(BindingResult bindingResult) {
+            final List<org.springframework.validation.FieldError> fieldErrors =
+                    bindingResult.getFieldErrors();
+            return fieldErrors.stream()
+                    .map(error -> new FieldError(
+                            error.getField(),
+                            error.getRejectedValue() == null ?
+                                    "" : error.getRejectedValue().toString(),
+                            error.getDefaultMessage()))
+                    .collect(Collectors.toList());
+        }
+
+    }
 }
