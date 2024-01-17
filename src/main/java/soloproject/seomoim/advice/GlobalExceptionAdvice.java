5cfd858ea48085e559adf19d24c1e9f24b5b5b99
서은정 <111293023+seoeunjeong@users.noreply.github.com>
2024-01-17 10:55:52 +0900

    사용하지않는 api 예외처리로직 삭제

--- src/main/java/soloproject/seomoim/advice/GlobalExceptionAdvice.java
+++ src/main/java/soloproject/seomoim/advice/GlobalExceptionAdvice.java
@@ -1,52 +1,19 @@
 package soloproject.seomoim.advice;
 
-import com.fasterxml.jackson.databind.exc.InvalidFormatException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import soloproject.seomoim.advice.exception.BusinessLogicException;
-import soloproject.seomoim.advice.exception.ExceptionCode;
 
 @RestControllerAdvice
 @Slf4j
 public class GlobalExceptionAdvice {
+
     @ExceptionHandler(IllegalStateException.class)
     public ResponseEntity<ErrorResponse> handleIllegalStateException(IllegalStateException e) {
         ErrorResponse errorResponse = ErrorResponse.of(HttpStatus.BAD_REQUEST, e.getMessage());
         return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
     }
-
-
-    @ExceptionHandler
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ErrorResponse handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
-        final ErrorResponse response = ErrorResponse.of(e.getBindingResult());
-
-        return response;
-    }
-
-    @ExceptionHandler
-    public ResponseEntity handleBusinessLogicException(BusinessLogicException e) {
-        ErrorResponse errorResponse = ErrorResponse.of(e.getExceptionCode());
-        return new ResponseEntity(errorResponse,HttpStatus.valueOf(errorResponse.getStatus()));
-    }
-
-    /*바인딩 실패 예외 api 처리 어떻게 해야할까*/
-    @ExceptionHandler
-    public ResponseEntity handleInvalidFormatException(InvalidFormatException e) {
-        log.info("e.getMessage={}",e.getMessage());
-        log.info("e.getValue()={}",e.getValue());
-        log.info("e.getValue()={}",e.getTargetType());
-
-        ErrorResponse.of(ExceptionCode.INVALID_REQUEST);
-
-        return new ResponseEntity(e.getMessage(), HttpStatus.BAD_REQUEST);
-    }
-
-
 
 }
